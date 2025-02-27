29c2ab9e26a0b681ece4d2f8d7b0d25482f0a91c
Praise Masunga <praisetechzw@gmail.com>
2025-02-27 21:43:24 +0200

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-// next.config.js
-module.exports = {
-  reactStrictMode: true,
-  // other configurations...
-};
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
